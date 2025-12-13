7d93b8b21620bfc57ad6546343fe463e508e6a14
Tushar-k-minny <tushark0134@gmail.com>
2025-12-14 00:23:41 +0530

    pino test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,11 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
 import { requestId } from "hono/request-id";
-import { pinoLogger } from "hono-pino";
 import { env } from "./env";
 import configureOpenAPI from "./lib/configure-open-api-app";
 import createApp from "./lib/create-app";
 import authRouter from "./modules/auth/index.auth";
 import healthRouter from "./modules/health";
-import { logger } from "./utils/logger";
 
 const app = new Hono();
 
@@ -16,16 +14,17 @@ configureOpenAPI(api);
 
 app.use(requestId());
 
-app.use(
-	"*",
-	pinoLogger({
-		pino: logger,
-	})
-);
+// app.use(
+// 	"*",
+// 	pinoLogger({
+// 		pino: logger,
+// 	})
+// );
+
 app.use("*", prettyJSON());
 
 app.get("/", (c) => {
-	logger.error("hello");
+	// logger.error("hello");
 	return c.text("Hello Hono!");
 });
 
@@ -37,7 +36,7 @@ for (const route of routes) {
 app.route("/", api);
 const port = Number(env.PORT ?? 5000);
 
-logger.info({ port }, "auth service listening");
+// logger.info({ port }, "auth service listening");
 
 export default {
 	port,
