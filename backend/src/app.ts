3f925f07513c7c5318bd8c02f0a5f27e3303c0e2
Tushar-k-minny <tushark0134@gmail.com>
2025-12-14 00:14:59 +0530

    test openapi instance

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,19 @@
 import { Hono } from "hono";
 import { env } from "./env";
+import configureOpenAPI from "./lib/configure-open-api-app";
+import createApp from "./lib/create-app";
 
 const testApp = new Hono();
+const api = createApp();
+configureOpenAPI(api);
 
 testApp.get("/", (c) => {
 	console.log(process.isBun);
 	return c.json({ message: "hello world", environmnet: env.NODE_ENV });
 });
+
+api.get("/api", (c) => c.text("Hello from API"));
+testApp.route("/", api);
 // testApp.route("/", app);
 
 export default {
