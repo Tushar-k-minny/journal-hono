07712ae66ce44ce3d5371f137d0de3b25792a405
Tushar-k-minny <tushark0134@gmail.com>
2025-12-14 17:52:29 +0530

    feat: rate limit implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 import { requestId } from "hono/request-id";
+import { rateLimiter } from "hono-rate-limiter";
 import { env } from "./env";
 import configureOpenAPI from "./lib/configure-open-api-app";
 import createApp from "./lib/create-app";
@@ -14,6 +15,14 @@ const app = new Hono();
 
 const api = createApp();
 configureOpenAPI(api);
+
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		limit: 100, // Limit each client to 100 requests per window
+		keyGenerator: (c) => c.req.header("x-forwarded-for") ?? "", // Use IP address as key
+	})
+);
 
 app.use(requestId());
 
